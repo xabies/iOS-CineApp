91a23311c8088de6a675c186dc2371b4995ba3a3
xabies <xabies@msn.com>
2015-09-01 17:02:59 +0200

    Final commit

---
+++ CineTests/CineTests.h
@@ -0,0 +1,13 @@
+//
+//  CineTests.h
+//  CineTests
+//
+//  Created by xabi on 26/08/2013.
+//  Copyright (c) 2013 Xabi. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CineTests : SenTestCase
+
+@end
---
+++ CineTests/CineTests.m
@@ -0,0 +1,32 @@
+//
+//  CineTests.m
+//  CineTests
+//
+//  Created by xabi on 26/08/2013.
+//  Copyright (c) 2013 Xabi. All rights reserved.
+//
+
+#import "CineTests.h"
+
+@implementation CineTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CineTests");
+}
+
+@end
